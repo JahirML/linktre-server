0ae00ccac9c26ee71341389d4b3c9e3098ad1d6a
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-06 21:46:11 -0600

    implement server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import server from "./server";
 
+import colors from "colors";
+
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log("Servidor funcionando", port);
+  console.log(colors.bgGreen(`Servidor funcionando: ${port}`));
 });
