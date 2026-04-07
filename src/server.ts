8ce6d0f3eee9e2fdec32d5d618e759dea490ee47
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-06 21:45:58 -0600

    create server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Router } from "express";
+import router from "./router";
+import "dotenv/config";
+import { connectDB } from "./config/db";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo express");
-});
+connectDB();
+// Leer datos
+app.use(express.json());
+
+app.use("/", router);
 
 export default app;
