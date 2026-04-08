637787d76561f3a079286ff6dbe0eb12b9da8331
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-07 21:57:33 -0600

    verify if user exists and hash  password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class UserController {
   static async createUser(req: Request, res: Response) {
     const user = new User(req.body);
+    const { email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      const error = new Error("This user already exists");
+      return res.status(409).json({ error: error.message });
+    }
 
     try {
+      user.password = await hashPassword(password);
       await user.save();
       res.send("Usuario creado correctamente");
     } catch (error) {
